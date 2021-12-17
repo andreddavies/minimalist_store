f5bfc8ccb96393003a479d8a141155bb5d5fde4c
Andre <andreddavies@hotmail.com>
2021-12-16 22:44:24 -0300

    Updating App to a class component and adding Redux + styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, Scandiweb Minimalist Store!</h1>
-    </div>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, Scandiweb Minimalist Store!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
