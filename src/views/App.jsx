d3d6a98b116838983c117ce365ecb82600fe6c2a
Andre <andreddavies@hotmail.com>
2021-12-18 23:26:16 -0300

    Creating App component

---
+++ src/views/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import * as S from "./App.styles";
+
+class App extends React.Component {
+  render() {
+    return (
+      <S.App>
+        <h1>Hello, Welcome to Minimalist Store!!!</h1>
+      </S.App>
+    );
+  }
+}
+
+export default App;
