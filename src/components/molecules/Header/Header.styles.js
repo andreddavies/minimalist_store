352d564c2c5fa610bef9f36102cb96b489da4986
Andre <andreddavies@hotmail.com>
2021-12-20 20:57:41 -0300

    Creating more styled-components and updating HeaderContainer

--- src/components/molecules/Header/Header.styles.js
+++ src/components/molecules/Header/Header.styles.js
@@ -3,6 +3,20 @@ import styled from "styled-components";
 const HeaderContainer = styled.header`
   width: 100%;
   height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
-export { HeaderContainer };
+const Box = styled.div`
+  width: 30%;
+  height: 100%;
+  background: blue;
+`;
+
+const Image = styled.img`
+  width: 41px;
+  height: 41px;
+`;
+
+export { HeaderContainer, Box, Image };
