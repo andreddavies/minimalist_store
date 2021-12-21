8c670554a599c51eb8ec9932472b6e35d97fbd6b
Andre <andreddavies@hotmail.com>
2021-12-20 22:57:07 -0300

    Creating a Button component

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -5,8 +5,16 @@ import * as S from "./Button.styles";
 class Button extends React.PureComponent {
   render() {
     return (
-      <S.Button type={this.props.type} onClick={this.props.onClick}>
-        {this.props.text}
+      <S.Button
+        type={this.props.type}
+        width={this.props.width}
+        height={this.props.height}
+        onClick={this.props.onClick}
+        btnStyle={this.props.btnStyle}
+        disabled={this.props.disabled}
+        background={this.props.background}
+      >
+        {this.props.text || this.props.children}
       </S.Button>
     );
   }
