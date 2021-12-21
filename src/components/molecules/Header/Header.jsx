11cc1f033bac440a7bd031238367e2a29e923fd7
Andre <andreddavies@hotmail.com>
2021-12-20 22:56:29 -0300

    Implementing static header for now

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
+import Tab from "../../atoms/Tab/Tab";
+import CartIcon from "../../../icons/CartIcon";
+
 import * as S from "./Header.styles";
 
 class Header extends React.PureComponent {
   render() {
     return (
       <S.HeaderContainer>
-        <h1>Header Component</h1>
+        <S.Box>
+          <Tab width="100%" isActive={true} text="Women" />
+        </S.Box>
+        <S.Box>
+          <S.Image src={`${process.env.PUBLIC_URL}/assets/logo.png`} />
+        </S.Box>
+        <S.Box>
+          <CartIcon width={20} height={20} />
+        </S.Box>
       </S.HeaderContainer>
     );
   }
