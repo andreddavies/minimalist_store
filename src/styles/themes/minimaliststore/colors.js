98fc287217b1b7172799f6ee38aa6210bb170390
Andre <andreddavies@hotmail.com>
2021-12-20 22:51:30 -0300

    Creating color scheme

---
+++ src/styles/themes/minimaliststore/colors.js
@@ -0,0 +1,28 @@
+export const colors = {
+  primary: "#FFF",
+  secondary: "#000",
+
+  buttons: {
+    primary: {
+      text: "#1D1F22",
+      border: "#1D1F22",
+      background: "#FFF",
+    },
+    secondary: {
+      text: "#FFF",
+      border: "#1D1F22",
+      background: "#1D1F22",
+    },
+    tertiary: {
+      text: "#FFF",
+      border: "#5ECE7B",
+      background: "#5ECE7B",
+    },
+
+    disabled: {
+      text: "#A6A6A6",
+      border: "#A6A6A6",
+      background: "#FFF",
+    },
+  },
+};
