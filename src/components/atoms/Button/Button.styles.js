48dedd986365019c85c30ede89aeeaeb6eb27cdf
Andre <andreddavies@hotmail.com>
2021-12-20 22:56:42 -0300

    Creating a Button component

--- src/components/atoms/Button/Button.styles.js
+++ src/components/atoms/Button/Button.styles.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+  background: ${({ theme, btnStyle }) =>
+    theme.colors.buttons[btnStyle].background};
   width: ${({ width }) => (!width && "279px") || `${width}`};
+  height: ${({ height }) => (!height && "52px") || `${height}`};
+  border: 1px solid
+    ${({ theme, btnStyle }) => theme.colors.buttons[btnStyle].border};
+  color: ${({ theme, btnStyle }) => theme.colors.buttons[btnStyle].text};
 `;
 
 export { Button };
