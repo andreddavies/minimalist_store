7b9c3774ad46acac47e9abff841fd8a9146e97ea
Andre <andreddavies@hotmail.com>
2021-12-21 21:21:38 -0300

    Creating text default theme

---
+++ src/styles/themes/minimaliststore/text.js
@@ -0,0 +1,15 @@
+export const text = {
+  size: {
+    text14: "14px",
+    text16: "16px",
+    text18: "18px",
+    text20: "20px",
+  },
+
+  weight: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+  },
+};
