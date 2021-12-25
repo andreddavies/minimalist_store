7d02a24111aabafe8ddb24307fc7daa600a1756b
Andre <andreddavies@hotmail.com>
2021-12-25 17:12:39 -0300

    Updating Container styles and creating a styled-component to Wrap children

--- src/components/organisms/PagesContainer/PagesContainer.styles.js
+++ src/components/organisms/PagesContainer/PagesContainer.styles.js
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 100%;
+  width: 95%;
   display: flex;
-  align-items: center;
-  flex-direction: column;
+  margin: 0 auto;
+  justify-content: center;
+
+  @media screen and (min-width: 768px) {
+    width: 85%;
+  }
 `;
 
-export { Container };
+const ChildrenWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export { Container, ChildrenWrapper };
