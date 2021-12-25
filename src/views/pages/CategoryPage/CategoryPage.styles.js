cf53c6f40e984d44058037eb2266fce307916d77
Andre <andreddavies@hotmail.com>
2021-12-25 17:14:14 -0300

    Creating title wrapper

--- src/views/pages/CategoryPage/CategoryPage.styles.js
+++ src/views/pages/CategoryPage/CategoryPage.styles.js
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
-const Container = styled.div`
+const TitleWrapper = styled.div`
   width: 100%;
+  margin: 60px 0;
+  font-size: 27px;
+  text-align: left;
+
+  @media screen and (min-width: 768px) {
+    margin: 80px 0;
+    font-size: 48px;
+  }
 `;
 
-export { Container };
+export { TitleWrapper };
