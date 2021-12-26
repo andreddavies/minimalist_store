7e4003139c5c40951c7cf42219fd24dbec76f924
Andre <andreddavies@hotmail.com>
2021-12-25 22:27:36 -0300

    Updating styles

--- src/views/pages/CategoryPage/CategoryPage.styles.js
+++ src/views/pages/CategoryPage/CategoryPage.styles.js
@@ -3,12 +3,13 @@ import styled from "styled-components";
 const TitleWrapper = styled.div`
   width: 100%;
   margin: 60px 0;
-  font-size: 27px;
-  text-align: left;
+  font-size: 1.7rem;
+  text-align: center;
 
   @media screen and (min-width: 768px) {
     margin: 80px 0;
-    font-size: 48px;
+    text-align: left;
+    font-size: 3.45rem;
   }
 `;
 
