046483caff5f9feb5c4a4b03116b98fb5cb1620b
Andre <andreddavies@hotmail.com>
2021-12-27 14:03:06 -0300

    Adding flex-wrap and margin styles

--- src/components/atoms/FlexContainer/FlexContainer.styles.js
+++ src/components/atoms/FlexContainer/FlexContainer.styles.js
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 const Container = styled.div`
     display flex;
-    margin: 0.75rem;
     width: ${({ width }) => `${width}`};
+    flex-wrap: ${({ wrap }) => `${wrap}`};
     align-items: ${({ align }) => `${align}`};
     justify-content: ${({ justify }) => `${justify}`};
     flex-direction: ${({ direction }) => `${direction}`};
+    margin: ${({ margin }) => (!margin && "0.75rem") || `${margin}`};
 
     ${({ styleProps }) => `${styleProps}`};
 `;
