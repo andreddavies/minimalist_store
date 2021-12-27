8015529cb43632a2967743c5f3c8842bae0af9aa
Andre <andreddavies@hotmail.com>
2021-12-27 14:02:12 -0300

    Adding width and width when screen min width it's 768px

--- src/components/organisms/PagesContainer/PagesContainer.styles.js
+++ src/components/organisms/PagesContainer/PagesContainer.styles.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 95%;
   display: flex;
   margin: 0 auto;
   justify-content: center;
+  width: ${({ width }) => (!width && "95%") || `${width}`};
 
   @media screen and (min-width: 768px) {
-    width: 85%;
+    width: ${({ tabletScreenWidth }) =>
+      (!tabletScreenWidth && "85%") || `${tabletScreenWidth}`};
   }
 `;
 
