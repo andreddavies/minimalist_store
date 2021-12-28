d23a993dcadf5bd88f59e7bb085b61142f03f381
Andre <andreddavies@hotmail.com>
2021-12-27 21:30:50 -0300

    Updating component and finishing product details

--- src/components/molecules/Product/Product.jsx
+++ src/components/molecules/Product/Product.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Button from "../../atoms/Button/Button";
 import FlexContainer from "../../atoms/FlexContainer/FlexContainer";
 
 import * as S from "./Product.styles";
@@ -8,8 +9,8 @@ class Product extends React.Component {
   render() {
     return (
       <FlexContainer
-        width="100%"
         wrap="wrap"
+        width="100%"
         margin="5.7rem 0"
         justify="flex-start"
       >
@@ -36,9 +37,95 @@ class Product extends React.Component {
           </S.ImageWrapper>
         </S.GalleryContainer>
         <S.ProductDetailsContainer>
-          <FlexContainer direction="column" align="flex-start">
-            <S.Title weight="600">Apollo</S.Title>
-            <S.Title weight="400">Running Short</S.Title>
+          <FlexContainer width="70%" direction="column">
+            <FlexContainer width="100%" direction="column" align="flex-start">
+              <S.Title weight="600">Apollo</S.Title>
+              <S.Title weight="400">Running Short</S.Title>
+            </FlexContainer>
+            <FlexContainer styleProps="margin-top: 3.07rem;">
+              <S.Title weight="700" size="1.29rem">
+                Size:
+              </S.Title>
+            </FlexContainer>
+            <FlexContainer
+              width="100%"
+              justify="space-between"
+              styleProps="max-width: 288px"
+            >
+              <Button
+                width="63px"
+                height="45px"
+                type="button"
+                disabled={false}
+                btnStyle="primary"
+                onClick={() => console.log("Hello")}
+              >
+                XS
+              </Button>
+              <Button
+                width="63px"
+                height="45px"
+                type="button"
+                disabled={false}
+                btnStyle="primary"
+                onClick={() => console.log("Hello")}
+              >
+                S
+              </Button>
+              <Button
+                width="63px"
+                height="45px"
+                type="button"
+                disabled={false}
+                btnStyle="primary"
+                onClick={() => console.log("Hello")}
+              >
+                M
+              </Button>
+              <Button
+                width="63px"
+                height="45px"
+                type="button"
+                disabled={false}
+                btnStyle="primary"
+                onClick={() => console.log("Hello")}
+              >
+                L
+              </Button>
+            </FlexContainer>
+            <FlexContainer
+              width="100%"
+              direction="column"
+              align="flex-start"
+              styleProps="margin-top: 2.85rem;"
+            >
+              <S.Title weight="700" size="1.29rem">
+                Price:
+              </S.Title>
+              <S.Title weight="700" size="1.7rem">
+                $50.00
+              </S.Title>
+            </FlexContainer>
+            <FlexContainer
+              width="100%"
+              direction="column"
+              justify="space-between"
+              styleProps="margin-top: 1.43rem;"
+            >
+              <Button
+                width="100%"
+                fontWeight="600"
+                fontSize="1.15rem"
+                btnStyle="tertiary"
+              >
+                ADD TO CART
+              </Button>
+              <S.Paragraph>
+                Find stunning women's cocktail dresses and party dresses. Stand
+                out in lace and metallic cocktail dresses and party dresses from
+                all your favorite brands.
+              </S.Paragraph>
+            </FlexContainer>
           </FlexContainer>
         </S.ProductDetailsContainer>
       </FlexContainer>
