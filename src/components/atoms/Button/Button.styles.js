200c63d07469b371e1b817b76c52a6765e0195a8
Andre <andreddavies@hotmail.com>
2021-12-27 21:29:00 -0300

    Adding outline style dynamic font-size and font-weight

--- src/components/atoms/Button/Button.styles.js
+++ src/components/atoms/Button/Button.styles.js
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 
 const Button = styled.button`
+  outline: 0;
   background: ${({ theme, btnStyle }) =>
     (btnStyle === "none" && "none") ||
     theme.colors.buttons[btnStyle].background};
+  font-size: ${({ fontSize }) => `${fontSize}`};
+  font-weight: ${({ fontWeight }) => `${fontWeight}`};
   border: ${({ theme, btnStyle }) =>
     (btnStyle === "none" && "none") ||
     `1px solid ${theme.colors.buttons[btnStyle].border}`};
   width: ${({ width }) => (!width && "279px") || `${width}`};
   height: ${({ height }) => (!height && "52px") || `${height}`};
   color: ${({ theme, btnStyle }) =>
-    (!btnStyle === "none" && theme.colors.buttons[btnStyle].text) || "none"};
+    (btnStyle === "none" && "none") || theme.colors.buttons[btnStyle].text};
 `;
 
 export { Button };
