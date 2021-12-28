2230db28513eaaf7f4a12a956fafacee9e3004fe
Andre <andreddavies@hotmail.com>
2021-12-27 21:30:01 -0300

    Updating styles

--- src/components/molecules/Product/Product.styles.js
+++ src/components/molecules/Product/Product.styles.js
@@ -32,6 +32,7 @@ const SmallImagesWrapper = styled.div`
   display: flex;
   overflow-x: scroll;
   margin-top: 0.75rem;
+  justify-content: space-evenly;
 
   & > img {
     height: 100%;
@@ -40,6 +41,7 @@ const SmallImagesWrapper = styled.div`
 
     @media screen and (min-width: 768px) {
       width: 100%;
+      margin-bottom: 1.28rem;
     }
   }
 
@@ -50,20 +52,39 @@ const SmallImagesWrapper = styled.div`
     overflow-x: auto;
     align-items: center;
     flex-direction: column;
+    justify-content: flex-start;
   }
 `;
 
 const ProductDetailsContainer = styled.div`
-  width: 40%;
+  width: 100%;
+  margin-top: 1.2rem;
+
+  @media screen and (min-width: 768px) {
+    width: 35%;
+    margin-top: 0;
+  }
 `;
 
 const Title = styled.h3`
-  font-size: 2.143rem;
   font-weight: ${({ weight }) => `${weight}`};
+  font-size: ${({ size }) => (!size && "1.5rem") || `${size}`};
+
+  @media screen and (min-width: 768px) {
+    font-size: ${({ size }) => (!size && "2.143rem") || `${size}`};
+  }
+`;
+
+const Paragraph = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  text-align: justify;
+  margin-top: 2.85rem;
 `;
 
 export {
   Title,
+  Paragraph,
   ImageWrapper,
   GalleryContainer,
   SmallImagesWrapper,
