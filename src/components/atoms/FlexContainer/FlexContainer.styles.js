3b3a45b16f67ebfb07f738d9798cf36ff12f1915
Andre <andreddavies@hotmail.com>
2021-12-29 12:30:32 -0300

    Creating a property to set styles when screen min width it's 768px. Setting dynamic styles to render if props are true

--- src/components/atoms/FlexContainer/FlexContainer.styles.js
+++ src/components/atoms/FlexContainer/FlexContainer.styles.js
@@ -2,14 +2,18 @@ import styled from "styled-components";
 
 const Container = styled.div`
     display flex;
-    width: ${({ width }) => `${width}`};
-    flex-wrap: ${({ wrap }) => `${wrap}`};
-    align-items: ${({ align }) => `${align}`};
-    justify-content: ${({ justify }) => `${justify}`};
-    flex-direction: ${({ direction }) => `${direction}`};
+    width: ${({ width }) => width && `${width}`};
+    flex-wrap: ${({ wrap }) => wrap && `${wrap}`};
+    align-items: ${({ align }) => align && `${align}`};
+    justify-content: ${({ justify }) => justify && `${justify}`};
     margin: ${({ margin }) => (!margin && "1rem 0") || `${margin}`};
+    flex-direction: ${({ direction }) => direction && `${direction}`};
 
-    ${({ styleProps }) => `${styleProps}`};
+    ${({ styleProps }) => styleProps && `${styleProps}`};
+
+    @media screen and (min-width: 768px){
+        ${({ tabletMinScreen }) => tabletMinScreen && `${tabletMinScreen}`};
+    }
 `;
 
 export { Container };
