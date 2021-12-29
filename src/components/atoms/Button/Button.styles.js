fdf674bf963be3dfceaf276e731fbcd0a22f515c
Andre <andreddavies@hotmail.com>
2021-12-29 12:28:58 -0300

    Updating style props and values

--- src/components/atoms/Button/Button.styles.js
+++ src/components/atoms/Button/Button.styles.js
@@ -3,18 +3,21 @@ import styled from "styled-components";
 const Button = styled.button`
   outline: 0;
   cursor: pointer;
+  color: ${({ theme, btnStyle }) =>
+    (btnStyle === "none" && "transparent") ||
+    theme.colors.buttons[btnStyle].text};
   background: ${({ theme, btnStyle }) =>
-    (btnStyle === "none" && "none") ||
+    (btnStyle === "none" && "transparent") ||
     theme.colors.buttons[btnStyle].background};
-  font-size: ${({ fontSize }) => `${fontSize}`};
-  font-weight: ${({ fontWeight }) => `${fontWeight}`};
   border: ${({ theme, btnStyle }) =>
-    (btnStyle === "none" && "none") ||
+    (btnStyle === "none" && "transparent") ||
     `1px solid ${theme.colors.buttons[btnStyle].border}`};
+  font-size: ${({ fontSize }) => fontSize && `${fontSize}`};
   width: ${({ width }) => (!width && "279px") || `${width}`};
   height: ${({ height }) => (!height && "52px") || `${height}`};
-  color: ${({ theme, btnStyle }) =>
-    (btnStyle === "none" && "none") || theme.colors.buttons[btnStyle].text};
+  font-weight: ${({ fontWeight }) => fontWeight && `${fontWeight}`};
+
+  ${({ styleProps }) => styleProps && `${styleProps}`};
 `;
 
 export { Button };
