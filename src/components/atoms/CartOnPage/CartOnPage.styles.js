4235bd9346dd717ece3c75a0b18e1c033023e8c6
Andre <andreddavies@hotmail.com>
2022-01-08 13:37:32 -0300

    Adding a styled-component

--- src/components/atoms/CartOnPage/CartOnPage.styles.js
+++ src/components/atoms/CartOnPage/CartOnPage.styles.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 
+import { Container } from "../FlexContainer/FlexContainer.styles";
+
+const Wrapper = styled(Container)`
+  border-top: 1px solid #e5e5e5;
+`;
+
 const Img = styled.img`
   width: 100%;
   height: 137px;
 
   @media screen and (min-width: 768px) {
-    max-width: 105px;
+    height: 185px;
+    max-width: 141px;
   }
 `;
 
-export { Img };
+export { Wrapper, Img };
