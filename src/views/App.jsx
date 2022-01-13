7bfa72c96f83fa762c849d3e657499c2ed0013ef
Andre <andreddavies@hotmail.com>
2022-01-13 13:14:39 -0300

    Connecting App with store using react-redux Provider

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { store } from "../store";
 import { ThemeDefault } from "../styles/theme";
 import { ThemeProvider } from "styled-components";
+import { Provider as ReduxProvider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Header from "../components/molecules/Header/Header";
@@ -11,14 +13,16 @@ import * as S from "./App.styles";
 class App extends React.Component {
   render() {
     return (
-      <ThemeProvider theme={ThemeDefault}>
-        <S.App>
-          <Router>
-            <Header />
-            <Content />
-          </Router>
-        </S.App>
-      </ThemeProvider>
+      <ReduxProvider store={store}>
+        <ThemeProvider theme={ThemeDefault}>
+          <S.App>
+            <Router>
+              <Header />
+              <Content />
+            </Router>
+          </S.App>
+        </ThemeProvider>
+      </ReduxProvider>
     );
   }
 }
