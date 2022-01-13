c82ce1f9626de69675a0e2b68b99159b04e266c9
Andre <andreddavies@hotmail.com>
2022-01-13 13:09:53 -0300

    Connecting Content component with store state to set if is showing cart overlay

--- src/components/organisms/Content/Content.jsx
+++ src/components/organisms/Content/Content.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 
 import Cart from "../../molecules/Cart/Cart";
@@ -6,14 +7,18 @@ import CartPage from "../../../views/pages/CartPage/CartPage";
 import ProductPage from "../../../views/pages/ProductPage/ProductPage";
 import CategoryPage from "../../../views/pages/CategoryPage/CategoryPage";
 
+import { store } from "../../../store";
+
 import * as S from "./Content.styles";
 
 class Content extends React.Component {
   render() {
+    const rootState = store.getState().store;
+
     return (
       <S.Main>
-        <Cart type="cartOverlay" isShowing={false} />
-        <S.BackgroundWall modalActive={false} />
+        <Cart type="cartOverlay" isShowing={rootState.cartOverlay} />
+        <S.BackgroundWall modalActive={rootState.cartOverlay} />
         <Routes>
           <Route exact path="/" element={<CategoryPage />} />
           <Route path="/product/:productId" element={<ProductPage />} />
@@ -24,4 +29,8 @@ class Content extends React.Component {
   }
 }
 
-export default Content;
+const mapState = (state) => ({
+  store: state.store,
+});
+
+export default connect(mapState, null)(Content);
