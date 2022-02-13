de9fbdfc3874b4fd6c74e061cc509ff82b71d1c0
Andre <andreddavies@hotmail.com>
2022-02-13 19:52:36 -0300

    Implementing method to get products

--- src/views/pages/CategoryPage/CategoryPage.jsx
+++ src/views/pages/CategoryPage/CategoryPage.jsx
@@ -1,24 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import ProductList from "../../../components/molecules/ProductList/ProductList";
 import PagesContainer from "../../../components/organisms/PagesContainer/PagesContainer";
 
+import { store } from "../../../store";
+import { GET_PRODUCTS } from "../../../services/queries/products";
+
 import * as S from "./CategoryPage.styles";
 
 class CategoryPage extends React.Component {
+  state = {
+    productsList: [],
+  };
+
+  componentDidMount() {
+    GET_PRODUCTS().then((result) => {
+      this.setState({
+        productsList: result.data.category.products,
+      });
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.state !== nextState || store.getState().store.currentCategory;
+  }
+
   render() {
-    const products = [
-      { title: "Product One", price: 2400, currency: "$", inStock: true },
-    ];
-    const categoryTitle = "Category One";
+    const rootState = store.getState().store;
 
     return (
       <PagesContainer>
-        <S.TitleWrapper>{categoryTitle}</S.TitleWrapper>
-        <ProductList products={products} />
+        <S.TitleWrapper>
+          {rootState.currentCategory.toUpperCase()}
+        </S.TitleWrapper>
+        <ProductList products={this.state.productsList} />
       </PagesContainer>
     );
   }
 }
 
-export default CategoryPage;
+const mapState = (state) => ({
+  store: state.store,
+});
+
+export default connect(mapState, null)(CategoryPage);
