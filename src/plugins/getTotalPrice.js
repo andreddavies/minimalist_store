0c2055b71ebdd782f5fce070cfba08daccf9b3d0
Andre <andreddavies@hotmail.com>
2022-02-13 19:44:14 -0300

    Creating a plugin to get cart total price

---
+++ src/plugins/getTotalPrice.js
@@ -0,0 +1,27 @@
+import { price } from "./masks";
+import { store } from "../store";
+
+const getTotalPrice = () => {
+  const rootState = store.getState().store;
+
+  const prices = rootState.cart.products.map((el) => {
+    if (el.quantity > 1) {
+      return (
+        el.prices.find((el) => el.currency.label === rootState.currency.label)
+          .amount * el.quantity
+      );
+    }
+    return el.prices.find(
+      (el) => el.currency.label === rootState.currency.label
+    ).amount;
+  });
+  const totalPrice = prices.reduce((a, b) => a + b, 0);
+
+  return (
+    (rootState.cart.products.length > 0 &&
+      price(totalPrice)[rootState.currency.label]()) ||
+    0
+  );
+};
+
+export default getTotalPrice;
