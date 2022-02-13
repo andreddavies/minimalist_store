1ab578fc431eae1b2df8184c63a81f2f102defa6
Andre <andreddavies@hotmail.com>
2022-02-13 19:49:51 -0300

    Updating component

--- src/components/atoms/CartFooter/CartFooter.jsx
+++ src/components/atoms/CartFooter/CartFooter.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Button from "../Button/Button";
 import Heading from "../Heading/Heading";
 import FlexContainer from "../FlexContainer/FlexContainer";
 
+import { store } from "../../../store";
+import getTotalPrice from "../../../plugins/getTotalPrice";
+
 class CartFooter extends React.Component {
+  shouldComponentUpdate() {
+    return true;
+  }
+
   render() {
+    const { dispatch } = store;
+
     return (
       <FlexContainer
         width="100%"
@@ -30,7 +40,7 @@ class CartFooter extends React.Component {
             color="primary"
             margin="0 0 0 1rem"
           >
-            $100.00
+            {getTotalPrice()}
           </Heading>
         </FlexContainer>
         <FlexContainer
@@ -38,7 +48,7 @@ class CartFooter extends React.Component {
           align="center"
           justify="space-between"
           tabletMinScreen={`
-            justify-content: flex-end;
+            ${!this.props.cartOverlay && "justify-content: flex-end"};
           `}
         >
           <Button
@@ -48,12 +58,22 @@ class CartFooter extends React.Component {
             fontSize="1rem"
             fontWeight="600"
             btnStyle="primary"
-            tabletMinScreen={`
-              display: none;
+            styleProps={`
+              ${!this.props.cartOverlay && "display: none"}
             `}
-            onClick={() => {}}
           >
-            VIEW BAG
+            <Link
+              to="/cart"
+              style={{
+                width: "100%",
+                height: "100%",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+              onClick={() => dispatch.store.setCartOverlay(false)}
+            >
+              VIEW BAG
+            </Link>
           </Button>
           <Button
             width="40%"
@@ -62,12 +82,22 @@ class CartFooter extends React.Component {
             fontSize="1rem"
             fontWeight="600"
             btnStyle="tertiary"
-            onClick={() => {}}
             tabletMinScreen={`
               width: 130px;
             `}
           >
-            CHECK OUT
+            <Link
+              to="/successfulPurchase"
+              style={{
+                width: "100%",
+                height: "100%",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+              onClick={() => dispatch.store.setCartOverlay(false)}
+            >
+              CHECK OUT
+            </Link>
           </Button>
         </FlexContainer>
       </FlexContainer>
