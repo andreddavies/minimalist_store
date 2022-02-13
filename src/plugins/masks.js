141ebb655f3540a5deedccc938b213130f8d99b4
Andre <andreddavies@hotmail.com>
2022-02-13 19:45:29 -0300

    Creating a plugin to mask prices depending on the currency

--- src/plugins/masks.js
+++ src/plugins/masks.js
@@ -1,3 +1,34 @@
-export const productPrice = (data) => {
-  return data.replace(/\D/g, "");
+export const price = (data) => {
+  return {
+    USD: () => {
+      return data.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    },
+    GBP: () => {
+      return data.toLocaleString("en-GB", {
+        style: "currency",
+        currency: "GBP",
+      });
+    },
+    AUD: () => {
+      return data.toLocaleString("en-US", {
+        style: "currency",
+        currency: "AUD",
+      });
+    },
+    JPY: () => {
+      return data.toLocaleString("ja-JP", {
+        style: "currency",
+        currency: "JPY",
+      });
+    },
+    RUB: () => {
+      return data.toLocaleString("ru-RU", {
+        style: "currency",
+        currency: "RUB",
+      });
+    },
+  };
 };
