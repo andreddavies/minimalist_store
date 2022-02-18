b41422ab30d955d02f18d15f0b102345baa62d69
Andre <andreddavies@hotmail.com>
2022-02-17 23:24:00 -0300

    Adding location props

--- src/hocs/withRouter.jsx
+++ src/hocs/withRouter.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 const withRouter = (WrappedComponent) => (props) => {
   const params = useParams();
+  const location = useLocation();
 
-  return <WrappedComponent {...props} params={params} />;
+  return <WrappedComponent {...props} params={params} router={location} />;
 };
 
 export default withRouter;
