4c535dc5f53c503ae1c375ded78408b95b85cf84
Andre <andreddavies@hotmail.com>
2022-02-21 20:38:43 -0300

    Adding property to set cart overlay to false when currency selected is on focus

--- src/components/atoms/Select/Select.jsx
+++ src/components/atoms/Select/Select.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
+import { store } from "../../../store";
+
 import * as S from "./Select.styles";
 
 class Select extends React.Component {
   render() {
+    const { dispatch } = store;
+    const rootState = store.getState().store;
+
     return (
-      <S.Select multiple={false} onChange={this.props.onChange}>
+      <S.Select
+        multiple={false}
+        onChange={this.props.onChange}
+        onFocus={() =>
+          rootState.cartOverlay && dispatch.store.setCartOverlay(false)
+        }
+      >
         {this.props.children}
       </S.Select>
     );
