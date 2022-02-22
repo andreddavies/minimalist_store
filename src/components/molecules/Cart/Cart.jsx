70a91f8f1f3607e99fea62536a02eaa54e3e9333
Andre <andreddavies@hotmail.com>
2022-02-21 22:24:33 -0300

    Removing inline styles and then unused props and variables

--- src/components/molecules/Cart/Cart.jsx
+++ src/components/molecules/Cart/Cart.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { connect } from "react-redux";
 
 import CartFooter from "../../atoms/CartFooter/CartFooter";
 import CartOnPage from "../../atoms/CartOnPage/CartOnPage";
 import CartOverlay from "../../atoms/CartOverlay/CartOverlay";
 
-import { store } from "../../../store";
-
 import * as S from "./Cart.styles";
 
 class Cart extends React.Component {
   render() {
-    const rootState = store.getState().store;
-
     return (
       <S.CartContainer isShowing={this.props.isShowing}>
         {(this.props.type === "cartOverlay" && (
@@ -21,13 +16,7 @@ class Cart extends React.Component {
           </CartOverlay>
         )) || (
           <CartOnPage>
-            <CartFooter
-              setToBottom={`
-                bottom: 0;
-                width: 90%;
-                position: ${rootState.cart.products.length === 0 && "fixed"};
-              `}
-            />
+            <CartFooter />
           </CartOnPage>
         )}
       </S.CartContainer>
@@ -35,8 +24,4 @@ class Cart extends React.Component {
   }
 }
 
-const mapState = (state) => ({
-  store: state.store,
-});
-
-export default connect(mapState, null)(Cart);
+export default Cart;
