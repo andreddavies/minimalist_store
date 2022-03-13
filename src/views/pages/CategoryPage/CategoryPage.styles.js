0d82d1ce600de18cc62f3443508a6f7dc0bfed06
Andre <andreddavies@hotmail.com>
2022-03-13 20:00:12 -0300

    Creating styled component

--- src/views/pages/CategoryPage/CategoryPage.styles.js
+++ src/views/pages/CategoryPage/CategoryPage.styles.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+import PagesContainer from "../../../components/organisms/PagesContainer/PagesContainer";
+
+const Container = styled(PagesContainer)`
+  justify-content: flex-start;
+`;
+
 const TitleWrapper = styled.div`
   width: 100%;
   margin: 60px 0;
@@ -7,10 +13,10 @@ const TitleWrapper = styled.div`
   text-align: center;
 
   @media screen and (min-width: 768px) {
-    margin: 80px 0;
     text-align: left;
     font-size: 3.45rem;
+    margin: 80px 0 40px 0;
   }
 `;
 
-export { TitleWrapper };
+export { Container, TitleWrapper };
