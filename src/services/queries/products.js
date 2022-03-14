8d3559e86d7cb88f146cb275ae91d0ac6a402aef
Andre <andreddavies@hotmail.com>
2022-03-13 22:03:15 -0300

    Removing async await, setting variables and adding a then callback

--- src/services/queries/products.js
+++ src/services/queries/products.js
@@ -1,39 +1,46 @@
 import { apiConfig } from "../apiConfig";
 import { gql } from "@apollo/client";
 
-const GET_PRODUCT = async (id) => {
-  return await apiConfig.query({
-    query: gql`
-        query{
-            product(id: "${id}"){
-                id
-                name
-                brand
-                inStock
-                gallery
-                category
-                description
-                prices{
-                    amount
-                    currency{
-                        label 
-                        symbol
-                    } 
-                } 
-                attributes{
-                    id
-                    name
-                    type
-                    items {
-                        id
-                        value
-                        displayValue
-                    }
-                }
+const GET_PRODUCT = (id) => {
+  return apiConfig
+    .query({
+      query: gql`
+        query ($productId: String!) {
+          product(id: $productId) {
+            id
+            name
+            brand
+            inStock
+            gallery
+            category
+            description
+            prices {
+              amount
+              currency {
+                label
+                symbol
+              }
             }
+            attributes {
+              id
+              name
+              type
+              items {
+                id
+                value
+                displayValue
+              }
+            }
+          }
         }
-    `,
-  });
+      `,
+      variables: {
+        productId: id,
+      },
+    })
+    .then((result) => {
+      return result.data;
+    });
 };
 
 const GET_PRODUCTS = (category) => {
