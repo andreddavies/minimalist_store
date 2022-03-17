8142606d8dc326e1733702c1a92442f9083cd353
Andre <andreddavies@hotmail.com>
2022-03-16 22:59:53 -0300

    Adding a hook to navigate programmatically

--- src/hocs/withRouter.jsx
+++ src/hocs/withRouter.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 
 const withRouter = (WrappedComponent) => (props) => {
   const params = useParams();
   const location = useLocation();
+  const navigate = useNavigate();
 
-  return <WrappedComponent {...props} params={params} router={location} />;
+  return (
+    <WrappedComponent
+      {...props}
+      params={params}
+      router={location}
+      navigate={navigate}
+    />
+  );
 };
 
 export default withRouter;
