6cc160ad95f557377547e03765a310ca76235308
Andre <andreddavies@hotmail.com>
2022-03-23 21:20:20 -0300

    Implementing new components, updating background to close cart overlay and updating z-index

--- src/components/organisms/Content/Content.jsx
+++ src/components/organisms/Content/Content.jsx
@@ -2,25 +2,32 @@ import React from "react";
 import { connect } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 
+import Alert from "../../atoms/Alert/Alert";
 import Cart from "../../molecules/Cart/Cart";
 import CartPage from "../../../views/pages/CartPage/CartPage";
 import ProductPage from "../../../views/pages/ProductPage/ProductPage";
 import CategoryPage from "../../../views/pages/CategoryPage/CategoryPage";
+import SuccessfulPurchase from "../../../views/pages/SuccessfulPurchase/SuccessfulPurchase";
 
 import * as S from "./Content.styles";
 
 class Content extends React.Component {
   render() {
-    const { cartOverlay } = this.props;
+    const { cartOverlay, setCartOverlay } = this.props;
 
     return (
       <S.Main>
+        <Alert />
         <Cart type="cartOverlay" isShowing={cartOverlay} />
-        <S.BackgroundWall modalActive={cartOverlay} />
+        <S.BackgroundWall
+          modalActive={cartOverlay}
+          onClick={() => setCartOverlay(false)}
+        />
         <Routes>
           <Route exact path="/" element={<CategoryPage />} />
           <Route path="/product/:productId" element={<ProductPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/successfulPurchase" element={<SuccessfulPurchase />} />
         </Routes>
       </S.Main>
     );
@@ -31,4 +38,8 @@ const mapState = (state) => ({
   cartOverlay: state.store.cartOverlay,
 });
 
-export default connect(mapState, null)(Content);
+const mapDispatch = (dispatch) => ({
+  setCartOverlay: dispatch.store.setCartOverlay,
+});
+
+export default connect(mapState, mapDispatch)(Content);
--- src/components/organisms/Content/Content.styles.js
+++ src/components/organisms/Content/Content.styles.js
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 const Main = styled.main`
   width: 100%;
+  height: 100%;
 `;
 
 const BackgroundWall = styled.div`
   width: 100%;
-  z-index: 99;
   height: 100%;
   opacity: 0.6;
+  z-index: 1000;
   position: fixed;
   background: ${({ theme }) => theme.colors.backgroundWall};
   display: ${({ modalActive }) => (modalActive && "block") || "none"};
