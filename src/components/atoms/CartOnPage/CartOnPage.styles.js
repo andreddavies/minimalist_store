e07c0ec95aed32f8b8d95011a3fd76aaa70f52e4
Andre <andreddavies@hotmail.com>
2022-03-23 21:22:02 -0300

    Creating styled-component to avoid inline style

--- src/components/atoms/CartOnPage/CartOnPage.styles.js
+++ src/components/atoms/CartOnPage/CartOnPage.styles.js
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 
+import { Button } from "../../atoms/Button/Button.styles";
 import { Container } from "../FlexContainer/FlexContainer.styles";
 
 const Wrapper = styled(Container)`
   border-top: 1px solid #e5e5e5;
+`;
+
+const AttributeButton = styled(Button)`
+  width: 63px;
+  height: 45px;
+  margin-right: 10px;
+  border: ${({ active }) => active && "4px solid #d3d3d3"};
+`;
+
+const SwatchFiller = styled.div`
+  width: 100%;
+  height: 100%;
+  background: ${({ backgroundColor }) => backgroundColor};
 `;
 
 const Img = styled.img`
@@ -16,4 +30,4 @@ const Img = styled.img`
   }
 `;
 
-export { Wrapper, Img };
+export { Wrapper, AttributeButton, Img, SwatchFiller };
